ae67617b050dc78b02710534bdcd679dfa315b6b
Pavlo Sharhan <pavel.shargan@gmail.com>
2024-10-04 09:17:05 -0400

    USB support

--- app/src/main/java/com/example/streamtest2/GoProHttpApi.kt
+++ app/src/main/java/com/example/streamtest2/GoProHttpApi.kt
@@ -2,7 +2,14 @@ package com.example.streamtest2
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.net.ConnectivityManager
+import android.net.Network
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withTimeoutOrNull
@@ -15,9 +22,12 @@ import kotlin.coroutines.resume
 object GoProHttpApi {
 
     private const val TAG = "GoProHttpApi"
-    private const val GOPRO_AP_BASE_URL = "http://10.5.5.9" // Replace with your base URL
-    private var baseUrl: String = GOPRO_AP_BASE_URL
-    private var globalOkHttpClient: OkHttpClient = OkHttpClient.Builder()
+    const val GOPRO_AP_BASE_URL = "http://10.5.5.9:8080/"
+    const val GOPRO_USB_BASE_URL = "http://172.2X.1YZ.51:8080/"  // where XYZ are the last three digits of the camera's serial number
+    const val GOPRO_NETWORK_ADDRESS =  "172.2X.1YZ.5"
+    
+    private var baseUrl: String = GOPRO_AP_BASE_URL //default for wifi
+    private var okHttpClient: OkHttpClient = OkHttpClient.Builder()
         .connectTimeout(10, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
         .build()
@@ -26,6 +36,96 @@ object GoProHttpApi {
 
     fun initialize(context: Context) {
         GoProHttpApi.context = context
+    }
+
+    fun mountGoproWired(){
+        val usbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
+        val deviceList: HashMap<String, UsbDevice> = usbManager.deviceList
+
+        for (device in deviceList.values) {
+            if (device.manufacturerName == "GoPro") {
+                if (!usbManager.hasPermission(device)) {
+                    Log.e( TAG, "Error: unsufficient permissions for usb device")
+//                        requestUsbPermission(context, device)
+                } else {
+                    // Handle the case where permission is already granted
+                    val serialNumber = device.serialNumber
+                    Log.e( TAG,"device details $device")
+                    Log.e( TAG,"GoPro $serialNumber connected")
+
+                    // GoPro serial number logic
+                    if (serialNumber != null) {
+                        goproWiredConnectedRoutines(serialNumber)
+                    }
+                }
+            } else {
+                println("Device is not a GoPro: ${device.manufacturerName}")
+            }
+        }
+    }
+
+    fun goproWiredConnectedRoutines(serialNumber: String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val goproNetworkAddess = GOPRO_NETWORK_ADDRESS
+                .replace("X", serialNumber[serialNumber.length - 3].toString())
+                .replace("Y", serialNumber[serialNumber.length - 2].toString())
+                .replace("Z", serialNumber[serialNumber.length - 1].toString())
+
+            baseUrl = GOPRO_USB_BASE_URL
+                .replace("X", serialNumber[serialNumber.length - 3].toString())
+                .replace("Y", serialNumber[serialNumber.length - 2].toString())
+                .replace("Z", serialNumber[serialNumber.length - 1].toString())
+
+            repeat(5) { attempt ->
+                val client = getGoProEthernetNetworkClient(context, goproNetworkAddess)
+                if (client != null) {
+                    okHttpClient = client
+                    setWiredMode(0) {
+                        setWiredMode(1) {}
+                    }
+                    return@launch
+                } else {
+                    if (attempt < 4) { // Avoid sleeping after the final attempt
+                        Thread.sleep(3000L) // Wait 3 seconds before trying again
+                    }
+                }
+            }
+        }
+    }
+
+    fun getGoProEthernetNetworkClient(context: Context, goproIpAddress: String): OkHttpClient? {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val allNetworks = connectivityManager.allNetworks
+        var selectedNetwork: Network? = null
+
+        // Loop through all networks to find the one with the GoPro IP address
+        for (network in allNetworks) {
+            val linkProperties = connectivityManager.getLinkProperties(network)
+
+            if (linkProperties != null) {
+                for (linkAddress in linkProperties.linkAddresses) {
+                    // Check if the network contains the GoPro IP address
+                    println("linkAddress.address.hostAddress ${linkAddress.address.hostAddress} goproIpAddress ${goproIpAddress}")
+                    if (linkAddress.address.hostAddress.contains(goproIpAddress)) {
+                        selectedNetwork = network
+                        break
+                    }
+                }
+                if (selectedNetwork != null) break
+            }
+        }
+
+        return if (selectedNetwork != null) {
+            // Create the OkHttpClient using a custom SocketFactory
+            OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .socketFactory( selectedNetwork.socketFactory)
+                .build()
+        } else {
+            null // Return null if the GoPro network is not found
+        }
     }
 
     // Kotlin-friendly blocking wrapper for the suspend function
@@ -39,15 +139,14 @@ object GoProHttpApi {
         return runBlocking { stopStream() }
     }
 
-    // Start streaming
     @JvmStatic
     suspend fun requestStream(): Boolean {
         val result = withTimeoutOrNull(10000L) { // Timeout after 10 seconds
             stopRecording() // Stop recording before starting the stream
             Thread.sleep(2000) // Wait to ensure the recording has stopped
             suspendCancellableCoroutine<Boolean> { continuation ->
-                val startStreamQuery = "$baseUrl/gopro/camera/stream/start"
-                makeGetRequest(globalOkHttpClient, startStreamQuery) { success, response ->
+                val startStreamQuery = "${baseUrl}gopro/camera/stream/start"
+                makeGetRequest(okHttpClient, startStreamQuery) { success, response ->
                     if (success) {
                         Log.i(TAG, "Stream started successfully")
                     } else {
@@ -60,13 +159,12 @@ object GoProHttpApi {
         return result ?: false
     }
 
-    // Stop streaming
     @JvmStatic
     suspend fun stopStream(): Boolean {
         val result = withTimeoutOrNull(10000L) { // Timeout after 10 seconds
             suspendCancellableCoroutine<Boolean> { continuation ->
-                val stopStreamQuery = "$baseUrl/gopro/camera/stream/stop"
-                makeGetRequest(globalOkHttpClient, stopStreamQuery) { success, response ->
+                val stopStreamQuery = "${baseUrl}gopro/camera/stream/stop"
+                makeGetRequest(okHttpClient, stopStreamQuery) { success, response ->
                     if (success) {
                         Log.i(TAG, "Stream stopped successfully")
                     } else {
@@ -79,7 +177,21 @@ object GoProHttpApi {
         return result ?: false
     }
 
-    // Make GET request
+    fun setWiredMode(value: Int, completion: () -> Unit) {
+        Log.e( TAG,"GPAPI setWiredMode..")
+        makeGetRequest(
+            okHttpClient,
+            "${baseUrl}gopro/camera/control/wired_usb?p=${value}"
+        ) { success, _ ->
+            if (success) {
+                Log.e( TAG,"GPAPI setWiredMode success")
+            } else {
+                Log.w( TAG,"GPAPI setWiredMode error")
+            }
+            completion()
+        }
+    }
+
     private fun makeGetRequest(client: OkHttpClient, url: String, callback: (Boolean, String?) -> Unit) {
         val request = Request.Builder()
             .url(url)
@@ -113,13 +225,22 @@ object GoProHttpApi {
         })
     }
 
+    fun startRecording() {
+        Log.i(TAG, "Starting recording...")
+        val stopRecordingQuery = "${baseUrl}gopro/camera/shutter/start"
+        makeGetRequest(okHttpClient, stopRecordingQuery) { success, response ->
+            if (success) {
+                Log.i(TAG, "Recording started successfully")
+            } else {
+                Log.e(TAG, "Failed to start recording")
+            }
+        }
+    }
 
-
-    // Stop recording (simplified example method)
-    private fun stopRecording() {
+    fun stopRecording() {
         Log.i(TAG, "Stopping recording...")
-        val stopRecordingQuery = "$baseUrl/gopro/camera/shutter/stop"
-        makeGetRequest(globalOkHttpClient, stopRecordingQuery) { success, response ->
+        val stopRecordingQuery = "${baseUrl}gopro/camera/shutter/stop"
+        makeGetRequest(okHttpClient, stopRecordingQuery) { success, response ->
             if (success) {
                 Log.i(TAG, "Recording stopped successfully")
             } else {
--- app/src/main/java/com/example/streamtest2/MainActivity.kt
+++ app/src/main/java/com/example/streamtest2/MainActivity.kt
@@ -1,10 +1,28 @@
 package com.example.streamtest2
 
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.FrameLayout
+import android.widget.Space
+import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -12,19 +30,104 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.streamtest2.ui.theme.StreamTest2Theme
 import com.google.android.exoplayer2.ui.PlayerView
+import kotlinx.coroutines.launch
 
 
 object DataStore {
     var livestreamEnabled by mutableStateOf(false)
 }
 
+class UsbReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.let {
+            when (it.action) {
+                UsbManager.ACTION_USB_DEVICE_ATTACHED -> {
+                    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                    device?.let {
+                        Log.d("USB ", "USB device attached: ${device.deviceName}")
+                        // Request permission for the attached device
+                        context?.let { ctx ->
+                            (ctx as? MainActivity)?.requestUsbPermission(context, device)
+                        }
+                    }
+                }
+                else -> {}
+            }
+        }
+    }
+}
 
-class MainActivity : androidx.activity.ComponentActivity() {
+class MainActivity : ComponentActivity() {
     private lateinit var streamHelper: StreamHelper
 
+    private val usbReceiver = UsbReceiver()
+    private val ACTION_USB_PERMISSION = "com.example.streamtest2.USB_PERMISSION"
+
+
+    fun requestUsbPermission(context: Context, device: UsbDevice) {
+        val usbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
+        val permissionIntent = PendingIntent.getBroadcast(
+            context,
+            0,
+            Intent(ACTION_USB_PERMISSION),
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        context.registerReceiver(permissionReceiver, filter, Context.RECEIVER_EXPORTED)
+        usbManager.requestPermission(device, permissionIntent)
+    }
+
+    private val permissionReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.let {
+                if (ACTION_USB_PERMISSION == it.action) {
+                    synchronized(this) {
+                        val device: UsbDevice? = it.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                        if (it.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                            device?.let {
+                                val serialNumber = it.serialNumber
+                                Toast.makeText(context, "Permissions granted for: $serialNumber", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            println("Permission denied for device $device")
+                        }
+                        context?.unregisterReceiver(this)  // Unregister the receiver after handling
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        println("OnNewIntent")
+        if (intent != null) {
+            super.onNewIntent(intent)
+        }
+        println("Intent action: ${intent?.action}")
+        if (intent?.action == UsbManager.ACTION_USB_DEVICE_ATTACHED) {
+            val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+            device?.let {
+                Log.d("USB", "Requesting permission for device: ${device.deviceName}")
+                GoProHttpApi.mountGoproWired()
+            }
+        }
+        else if (intent?.action != null){
+            val flags = intent?.flags
+            if (flags != null) {
+                if ((flags and Intent.FLAG_ACTIVITY_CLEAR_TOP == Intent.FLAG_ACTIVITY_CLEAR_TOP) &&
+                    (flags and Intent.FLAG_ACTIVITY_CLEAR_TASK == Intent.FLAG_ACTIVITY_CLEAR_TASK)) {
+                    setIntent(intent)
+                } else {
+                    finish() // Finish this activity if another instance is found
+                }
+            }
+        }
+
+    }
 
     @Composable
     fun LivestreamView(streamHelper: StreamHelper) {
@@ -96,23 +199,51 @@ class MainActivity : androidx.activity.ComponentActivity() {
 
         GoProHttpApi.initialize(this)
 
+        val filter = IntentFilter()
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+        registerReceiver(usbReceiver, filter)
+
         streamHelper = StreamHelper(this)
         streamHelper.stopStream()
-
-        // Set up Jetpack Compose content
         setContent {
+            val scope =
+                rememberCoroutineScope() // Create a coroutine scope to handle suspend functions
+
             StreamTest2Theme {
                 if (DataStore.livestreamEnabled) {
                     LivestreamView(streamHelper)
                 } else {
-                    Button(onClick = { toggleLivestream() }) {
-                        Text("Start Stream")
+                    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+                        Button(onClick = { toggleLivestream() }) {
+                            Text("Start Stream")
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            Button(onClick = {
+                                scope.launch {
+                                    GoProHttpApi.startRecording() // Call startRecording in a coroutine
+                                }
+                            }) {
+                                Text("Start Recording")
+                            }
+
+                            Button(onClick = {
+                                scope.launch {
+                                    GoProHttpApi.stopRecording() // Call stopRecording in a coroutine
+                                }
+                            }) {
+                                Text("Stop Recording")
+                            }
+                        }
                     }
                 }
             }
         }
-
-        // Start streaming
     }
 
 
@@ -120,5 +251,14 @@ class MainActivity : androidx.activity.ComponentActivity() {
         super.onDestroy()
         streamHelper.stopStream()
         streamHelper.release()
+
+        if(this::streamHelper.isInitialized){
+            streamHelper.release()
+        }
+        try {
+            unregisterReceiver(usbReceiver)
+        } catch (e: Exception) {
+            Log.e("USB","Error while unregistering receiver: ${e.message}")
+        }
     }
 }
